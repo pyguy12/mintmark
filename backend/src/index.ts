485e62a6ebd23289b8d5f59edbf491a2d184bd34
Warren Ste Wu <wuwarren7360@gmail.com>
2023-06-06 16:55:14 -0500

    Got connection to MongoDB working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,31 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import { run as connectToDB } from './databaseConnection';
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// Define an async function for starting the server
+async function startServer() {
+    try {
+        // Attempt to connect to the database
+        await connectToDB();
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+        // Define your routes
+        app.get('/', (req, res) => {
+            res.send('Hello World!');
+        });
+
+        // Start the server only if the database connection is successful
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    } catch (err) {
+        console.error('Failed to connect to database, server not started', err);
+        process.exit(1); // Exit the process with a "failure" code
+    }
+}
+
+// Call the async function to start the server
+startServer();
